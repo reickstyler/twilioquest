c7b88b6f07e2f3334d9291ad1f94de8ac5dc53f9
Tyler Reicks <tyler.reicks@gmail.com>
2021-02-21 22:59:23 -0700

    Episode 8

---
+++ freightMass.js
@@ -0,0 +1,18 @@
+function calculateMass(freightItems) {
+  // This is a placeholder for the total length in characters of all strings
+  // in the freightItems array
+  let totalMass = 0;
+
+  freightItems.reduce((acc, item) => {
+    acc += item.length;
+    totalMass = acc;
+    return acc;
+  }, 0);
+
+  return totalMass;
+}
+
+// The following lines of code are not required for the solution, but can be
+// used by you to test your solution.
+const mass = calculateMass(["dog", "donkey", "cat"]);
+console.log("Total mass of items is " + mass); // should be 12
